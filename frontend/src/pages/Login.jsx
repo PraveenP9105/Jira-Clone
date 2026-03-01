9718ac8c519d6996fad5dd83f9fa5453059a4055
PraveenP9105 <2k22cse117@kiot.ac.in>
2026-03-01 11:40:41 +0530

    Updated frontend API URL

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import axios from "axios";
+import api from "../api/axios";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
@@ -11,8 +11,7 @@ function Login() {
         e.preventDefault();
 
         try {
-            const res = await axios.post(
-                "http://localhost:5000/api/auth/login",
+            const res = await api.post("/auth/login",
                 { email, password }
             );
 
